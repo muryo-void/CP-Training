980004212c3a6fed774db32329b5527c94551157
muryo-void <200528615+muryo-void@users.noreply.github.com>
2026-04-29 23:07:05 +0200

    Templates: Erstelle Standard-Template

---
+++ Templates/template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Abkürzungen & Makros ---
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+#define sz(a) int((a).size())
+#define all(c) (c).begin(), (c).end()
+#define pb push_back
+
+void solve() {
+    // Lösung
+}
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Mehrere Testcases
+    int t = 1;
+    cin >> t; // Bei nur einem Testcase auskommentiere
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
